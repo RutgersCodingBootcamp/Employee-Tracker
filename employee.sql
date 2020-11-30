5e6d34181c60b47209426ab3e0809c9be04e8b32
Christopher-Lamb <chris.lamb13@hotmail.com>
2020-11-29 18:44:45 -0800

    MySQL set up with VSCode

---
+++ employee.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee {
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT
+}
+
+CREATE TABLE role{
+ id INT NOT NULL AUTO_INCREMENT,
+ title VARCHAR(30),
+ salary DECIMAL
+ department_id INT
+}
+
+CREATE TABLE department {
+ id INT NOT NULL AUTO_INCREMENT,
+ name VARCHAR(30)
+}
